2f618f478cee498fd295227e2d202b50e757e086
Ertan Turan <ertanturan@outlook.com>
2020-11-29 13:53:41 +0300

    connecting the backend

--- FrontEnd.py
+++ FrontEnd.py
@@ -13,7 +13,34 @@ User can:
     """
 
 from tkinter import *
+
 import BackEnd
+
+
+def view_command():
+    listOne.delete('0', END)
+    for row in BackEnd.view():
+        listOne.insert(END, row)
+
+def insert_command():
+    BackEnd.insert(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+
+
+def search_command():
+    listOne.delete('0', END)
+    values = BackEnd.search(title_text.get(),author_text.get(),year_text.get(),isbn_text.get())
+    for value in values:
+        listOne.insert(END, value)
+
+
+def get_selected_row(event):
+    index = listOne.curselection()[0]
+    selectedTuple = listOne.get(index)
+    return selectedTuple
+
+
+def delete_command():
+    BackEnd.delete(get_selected_row()[0])
 
 window = Tk()
 
@@ -48,25 +75,27 @@ entryOne.grid(row=1, column=3)
 listOne = Listbox(window, height=6, width=35)
 listOne.grid(row=2, column=0, rowspan=6, columnspan=2)
 
+listOne.bind('<<ListboxSelect>>',get_selected_row)
+
 scrollBarOne = Scrollbar(window)
 scrollBarOne.grid(row=2, column=2, rowspan=6)
 
 listOne.configure(yscrollcommand=scrollBarOne.set)
 scrollBarOne.configure(command=listOne.yview)
 
-viewAllButton = Button(window, text="View All", width=12)
+viewAllButton = Button(window, text="View All", width=12, command=view_command)
 viewAllButton.grid(row=2, column=3)
 
-searchEntryButton = Button(window, text="Search Entry", width=12)
+searchEntryButton = Button(window, text="Search Entry", width=12,command = search_command)
 searchEntryButton.grid(row=3, column=3)
 
-addEntryButton = Button(window, text="Add Entry", width=12)
+addEntryButton = Button(window, text="Add Entry", width=12,command=insert_command)
 addEntryButton.grid(row=4, column=3)
 
 updateSelectedButton = Button(window, text="Update Selected", width=12)
 updateSelectedButton.grid(row=5, column=3)
 
-deleteSelectedButton = Button(window, text="Delete Selected", width=12)
+deleteSelectedButton = Button(window, text="Delete Selected", width=12 , command = delete_command)
 deleteSelectedButton.grid(row=6, column=3)
 
 closeButton = Button(window, text="Close", width=12)
